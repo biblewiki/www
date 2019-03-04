184d95c5bf122e3ceadc73482e4b38880db9b557
Joel Kohler <jk@joelkohler.ch>
2019-03-04 21:17:48 +0100

    Added Toastr Notifications

--- login/index.php
+++ login/index.php
@@ -48,25 +48,28 @@ HTML;
 }
 echo <<<HTML
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Login - BibleWiki</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet" />
+    <link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="style.css" rel="stylesheet" />
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js" ></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <script src="script.js"></script>
 
     <!-- Bootstrap start -->
-      <title>Login Biblewiki</title>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+      <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"-->
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <!-- Bootstrap stop -->
     
-    <!--link rel="stylesheet" type="text/css" media="screen" href="main.css">
-    <script src="main.js"></script-->
 </head>
 <body>
   <div class="container">
@@ -81,6 +84,10 @@ echo <<<HTML
 
       </form>   
     </center>
+
+    <button class="btn btn-primary" id="tryMe">Try Me</button>        
+
+
   </div>
 </body>
 </html>
